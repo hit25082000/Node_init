2cdb4386ed8373923256bd5699225fe8b1473d07
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2021-12-16 15:38:31 -0400

    Node_init: criando rotas e utilizando requisições http

--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return response.json({mensagem: "ola"})
+/*
+GET - Buscar informação dentro do servidor
+POST - Inserir uma informação no servidor
+PUT - Alterar uma informação no servidor
+PATCH - Alterar uma informação especifica no servidor
+DELETE - Deletar uma informação no servidor
+*/
+
+app.get("/courses", (request, response) => {
+    return response.json([
+        "curso1",
+        "curso2",
+        "curso3",        
+    ])
 })
 
-app.listen(6666)
+app.post("/courses", (request, response) => {
+    return response.json([
+        "curso1",
+        "curso2",
+        "curso3",        
+        "curso4",        
+    ])
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json([
+        "curso6",
+        "curso2",
+        "curso3",        
+        "curso4",        
+    ])
+})
+
+app.path("/courses/:id", (request, response) => {
+    return response.json([
+        "curso6",
+        "curso7",
+        "curso3",        
+        "curso4",        
+    ])
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json([
+        "curso6",
+        "curso2",             
+        "curso4",        
+    ])
+})
+
+
+app.listen(3000);
