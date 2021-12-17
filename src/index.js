378a953822a820c5c48f4f65e1e9953c7882aa8e
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2021-12-17 14:02:48 -0400

    Node_init: midleware

--- src/index.js
+++ src/index.js
@@ -8,12 +8,21 @@ app.use(express.json());
 const customers = [];
 //Zera toda x que o codigo é reiniciado (nodemon reinicia o codigo sempre que ha um alteração)
 
-/*
-    cpf - string
-    name - string
-    id - uuid
-    statement [] 
-*/
+//Midleware
+function verifyIfExistAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({error:"Customer not found"})
+    }
+
+    request.customer = customer;
+
+    return next();
+
+}
 app.post("/account", (request, response) => {
     const { cpf , name } = request.body;
 
@@ -33,17 +42,13 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 })
 
-app.get("/statement", (request, response) => {
-    const { cpf } = request.headers;
+//app.use(verifyIfExistAccountCPF)
+//Verifica sempre com  midleware
 
-    const customer = customers.find(customer => customer.cpf === cpf);
-    //Find retorna o dado se existente completo
-    if(!customer) {
-        return response.status(400).json({error:"Customer not found"})
-    }
-
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
+    //Verifica somente nesta rota
     return response.json(customer.statement);
-
 })
 
 app.listen(3000);
